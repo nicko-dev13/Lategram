4f0ae92ede057547add0ed6f5ceb32be154a5de7
nikhil <nikhil13012000@gmail.com>
2020-03-27 15:13:35 +0530

    writing comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Comment = require('../models/Comments');
 
+//@route POST api/comments
+//@desc Posting comments to user posts
+//@access Private
 router.post('/', async (req, res) => {
 	try {
+		//destructuring
 		const { username, comment } = req.body;
+		//assigning
 		const newComment = new Comment({
 			username,
 			comment
 		});
-
+		//saving to database
 		const saveComment = await newComment.save();
+		//sending result
 		res.json(saveComment);
 	} catch (error) {
 		console.log(error);
