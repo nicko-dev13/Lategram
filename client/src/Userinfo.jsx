25730d75bfb0de1212e6ab646f2c1a52dbe62069
ShivPrashant777 <shivprashant777@gmail.com>
2020-03-29 14:06:19 +0530

    Add View Post
    View Post Image Aspect Ratio to be fixed

--- client/src/Userinfo.jsx
+++ client/src/Userinfo.jsx
@@ -17,6 +17,31 @@ function Userinfo() {
             </div>
             <hr />
             <PostsComponent />
+            <div className="view-post">
+                <div className="view-post-container">
+                    <div className="image">
+                        <img src={require("./Cod.jpg")} />
+                    </div>
+                    <div className="author">
+                        <Userimage width="50px" />
+                        <span> UserNAME </span>
+                    </div>
+                    <div className="comments">
+                        <span> <b>99</b> posts  </span>
+                        <span>  <b>420</b> followers  </span>
+                        <span>  <b>69</b> following  </span>
+                        <p>Also known as pako and cko and mako and kako and taco</p>
+                    </div>
+                    <div className="author">
+                        <Userimage width="50px" />
+                        <span> UserNAME </span>
+                    </div>
+                    <div className="author">
+                        <Userimage width="50px" />
+                        <span> UserNAME </span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
