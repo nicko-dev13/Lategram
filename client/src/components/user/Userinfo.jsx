5dd477dc260332dc981d162b212c4ed2612742ac
ShivPrashant777 <shivprashant777@gmail.com>
2020-04-17 23:20:29 +0530

    Add Posts as array to Users
    Posts now stored as array in Users collection

--- client/src/components/user/Userinfo.jsx
+++ client/src/components/user/Userinfo.jsx
@@ -1,36 +1,36 @@
-import React, { useContext } from 'react';
-import Userimage from './Userimage';
-import AuthContext from '../../context/auth/authContext';
-import PostsComponent from '../post/PostsComponent';
+import React, { useContext } from "react";
+import Userimage from "./Userimage";
+import AuthContext from "../../context/auth/authContext";
+import PostsComponent from "../displayPosts/PostsComponent";
 
 function Userinfo() {
-	const authContext = useContext(AuthContext);
+  const authContext = useContext(AuthContext);
 
-	const { user } = authContext;
+  const { user } = authContext;
 
-	return (
-		<div className='user-info'>
-			<div className='info-header'>
-				<Userimage width='15vw' />
-				<div className='info'>
-					<p>{user.name}</p>
-					<span>
-						{' '}
-						<b>99</b> posts{' '}
-					</span>
-					<span>
-						{' '}
-						<b>420</b> followers{' '}
-					</span>
-					<span>
-						{' '}
-						<b>69</b> following{' '}
-					</span>
-					<p>Also known as pako and cko and mako and kako and taco</p>
-				</div>
-			</div>
-			<PostsComponent />
-			{/* <div className="view-post">
+  return (
+    <div className="user-info">
+      <div className="info-header">
+        <Userimage width="15vw" />
+        <div className="info">
+          <p>{user.name}</p>
+          <span>
+            {" "}
+            <b>99</b> posts{" "}
+          </span>
+          <span>
+            {" "}
+            <b>420</b> followers{" "}
+          </span>
+          <span>
+            {" "}
+            <b>69</b> following{" "}
+          </span>
+          <p>Also known as pako and cko and mako and kako and taco</p>
+        </div>
+      </div>
+      <PostsComponent />
+      {/* <div className="view-post">
 		            <div className="view-post-container">
 		                <div className="image">
 		                    <img src={require("./Cod.jpg")} />
@@ -55,8 +55,8 @@ function Userinfo() {
 		                </div>
 		            </div>
 		        </div> */}
-		</div>
-	);
+    </div>
+  );
 }
 
 export default Userinfo;
--- routes/auth.js
+++ routes/auth.js
@@ -1,81 +1,81 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const auth = require('../middlewares/auth');
-const { validationResult, check } = require('express-validator');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../middlewares/auth");
+const { validationResult, check } = require("express-validator");
 
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 //@route get api/auth
 //@desc Retrieves a user from db
 //@access Private
-router.get('/', auth, async (req, res) => {
-	try {
-		//Finds the user from database
-		const user = await User.findById(req.user.id).select('-password');
-		res.json(user);
-	} catch (error) {
-		console.log(error.msg);
-		res.status(500).json({ msg: 'Server Error' });
-	}
+router.get("/", auth, async (req, res) => {
+  try {
+    //Finds the user from database
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.msg);
+    res.status(500).json({ msg: "Server Error" });
+  }
 });
 
 //@route POST api/auth
 //@desc authorize a user and get token (login into app)
 //@access Public
 router.post(
-	'/',
-	[
-		check('email', 'Please Enter a valid email').isEmail(),
-		check('password', 'Please enter your password').exists()
-	],
-	async (req, res) => {
-		//Checking the validation errors
-		const error = validationResult(req);
-		if (!error.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-		//Destructuring
-		const { email, password } = req.body;
+  "/",
+  [
+    check("email", "Please Enter a valid email").isEmail(),
+    check("password", "Please enter your password").exists(),
+  ],
+  async (req, res) => {
+    //Checking the validation errors
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: error.array() });
+    }
+    //Destructuring
+    const { email, password } = req.body;
 
-		//Checking if the user exists or not else passing error
-		try {
-			const user = await User.findOne({ email: email });
+    //Checking if the user exists or not else passing error
+    try {
+      const user = await User.findOne({ email: email });
 
-			if (!user) {
-				return res.status(400).json({
-					msg: 'Invalid Credentials'
-				});
-			}
-			//comparing passwords
-			const isMatch = await bcrypt.compare(password, user.password);
+      if (!user) {
+        return res.status(400).json({
+          msg: "Invalid Credentials",
+        });
+      }
+      //comparing passwords
+      const isMatch = await bcrypt.compare(password, user.password);
 
-			if (!isMatch) {
-				return res.status(400).json({
-					msg: 'Invalid Credentials'
-				});
-			}
-			//getting a jwt token
-			const payload = {
-				user: {
-					id: user.id
-				}
-			};
+      if (!isMatch) {
+        return res.status(400).json({
+          msg: "Invalid Credentials",
+        });
+      }
+      //getting a jwt token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-			jwt.sign(payload, config.get('jwtSecret'), (err, token) => {
-				if (err) throw err;
-				res.json({ token });
-			});
-		} catch (error) {
-			console.log(error);
-			res.status(500).json({
-				msg: 'Server Error'
-			});
-		}
-	}
+      jwt.sign(payload, config.get("jwtSecret"), (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        msg: "Server Error",
+      });
+    }
+  }
 );
 
 module.exports = router;
