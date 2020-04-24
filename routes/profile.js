f81df74923a156f0b0fc96e978345807f7621e96
ShivPrashant777 <shivprashant777@gmail.com>
2020-04-24 14:34:28 +0530

    Add condtions for followers
    Cannot follow again and follow themself

--- routes/profile.js
+++ routes/profile.js
@@ -8,11 +8,36 @@ const Profile = require('../models/Profile');
 //@desc Follow A User
 //@access Private
 router.post('/follow/:id', auth, async (req, res) => {
-	var getFollower = await User.findOne({ _id: req.params.id });
-	follower = {
-		follower_id: getFollower._id,
-	};
+	// try {
+	// 	var getFollower = await User.findOne({ _id: req.params.id });
+	// 	const follower = {
+	// 		follower_id: getFollower._id,
+	// 	};
+	// } catch (error) {
+	// 	console.log(error.message);
+	// 	res.status(500).send('No Such User Exists');
+	// }
 	try {
+		var user = await Profile.findOne({ user_id: req.user.id });
+		if (
+			user.followers.filter(
+				(follower) => follower.follower_id == req.params.id
+			).length > 0
+		) {
+			return res.status(400).json({ msg: 'Already following' });
+		}
+		if (user.user_id == req.params.id) {
+			return res.status(400).json({ msg: 'Cannot follow yourself' });
+		}
+		// check whether user is following themself
+	} catch (error) {
+		console.log(error.message);
+		res.status(500).send('Error Updating Users');
+	}
+	try {
+		const follower = {
+			follower_id: req.params.id,
+		};
 		Profile.updateOne(
 			{ user_id: req.user.id },
 			{ $push: { followers: follower } },
@@ -27,7 +52,7 @@ router.post('/follow/:id', auth, async (req, res) => {
 		);
 	} catch (error) {
 		console.log(error.message);
-		res.status(500).send('Error Fetching Users');
+		res.status(500).send('Error Updating Users');
 	}
 });
 
