6c632424dadd2c1d66abd89160eaab3e46e79800
nikhil <nikhil13012000@gmail.com>
2020-04-25 13:40:36 +0530

    display likes

--- client/src/components/displayPosts/PostsComponent.jsx
+++ client/src/components/displayPosts/PostsComponent.jsx
@@ -2,8 +2,13 @@ import React, { useContext, useEffect } from 'react';
 import PostContext from '../../context/posts/postContext';
 import postContext from '../../context/posts/postContext';
 
-function PostsComponent({ post: { postContent } }) {
-    return <div className='post-container'>{postContent}</div>;
+function PostsComponent({ post: { postContent, likes } }) {
+    return (
+        <div className='post-container'>
+            <h4>{postContent}</h4>
+            <p>likes {likes.length}</p>
+        </div>
+    );
 }
 
 export default PostsComponent;
