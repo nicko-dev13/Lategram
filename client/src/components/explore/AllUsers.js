c01baf6502292d25a630100f73750c2f45c2ab93
ShivPrashant777 <shivprashant777@gmail.com>
2020-04-25 20:55:43 +0530

    Change following Route

--- client/src/components/explore/AllUsers.js
+++ client/src/components/explore/AllUsers.js
@@ -5,17 +5,22 @@ import UserItem from './UserItem';
 
 function AllUsers() {
 	const userContext = useContext(UserContext);
-	const { users, getUsers } = userContext;
+	const { users, following, getUsers, getFollowers } = userContext;
 
 	useEffect(() => {
 		getUsers();
+		getFollowers();
 		// eslint-disable-next-line
 	}, []);
+
+	const others = users.filter(
+		(o) => following.map((i) => i._id).indexOf(o._id) == -1
+	);
 
 	return (
 		<div>
 			{users.map((user) => (
-				<UserItem key={user._id} user={user} />
+				<UserItem key={user.id} user={user} button="Unfollow" />
 			))}
 		</div>
 	);
