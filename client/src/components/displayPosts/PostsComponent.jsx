fc4e79ec4b80b66ceca8c5acedaa5a99b159f928
ShivPrashant777 <shivprashant777@gmail.com>
2020-04-26 15:13:14 +0530

    Fix Posts Grid

--- client/src/components/displayPosts/PostsComponent.jsx
+++ client/src/components/displayPosts/PostsComponent.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
 function PostsComponent({ post: { postContent, likes } }) {
-    const onLike = () => {
-        console.log('OnLike');
-    };
+	const onLike = () => {
+		console.log('OnLike');
+	};
 
-    return (
-        <div className='post-container'>
-            <div className="flexo">
-                    <div><h4>{postContent}</h4></div> 
-                    <div><button id="btn" onClick={onLike}>Likes {likes.length}</button></div> 
-            </div>
-                   
-        </div>
-    );
+	return (
+		<div className="post">
+			<h4>{postContent}</h4>
+			<button id="btn" onClick={onLike}>
+				Likes {likes.length}
+			</button>
+		</div>
+	);
 }
 
 export default PostsComponent;
--- client/src/components/user/Userinfo.jsx
+++ client/src/components/user/Userinfo.jsx
@@ -7,46 +7,55 @@ import postContext from '../../context/posts/postContext';
 import Spinner from '../layout/Spinner';
 
 function Userinfo() {
-    const authContext = useContext(AuthContext);
+	const authContext = useContext(AuthContext);
 
-    const PostContext = useContext(postContext);
+	const PostContext = useContext(postContext);
 
-    const { getPosts, loading, posts } = PostContext;
+	const { getPosts, loading, posts } = PostContext;
 
-    useEffect(() => {
-        getPosts();
-        // eslint-disable-next-line
-    }, []);
+	useEffect(() => {
+		getPosts();
+		// eslint-disable-next-line
+	}, []);
 
-    const { user } = authContext;
+	const { user } = authContext;
 
-    return (
-        <div className='user-info'>
-            <div className='info-header'>
-                <Userimage width='5vw' />
-                <div className='info'>
-                    <p>{user.name}</p><br/><br/>
-                    <span>
-                        <b>99</b><p>posts{' '}</p> 
-                    </span>
-                    <span>
-                        <b>420</b><p>followers{' '}</p> 
-                    </span>
-                    <span>
-                        <b>69</b><p> following{' '}</p>
-                    </span><br/><br/>
-                    <p>Also known as pako and cko and mako and kako and taco</p>
-                </div>
-            </div>
-            {!loading && posts !== null ? (
-                posts.map((post) => (
-                    <PostsComponent key={post._id} post={post} />
-                ))
-            ) : (
-                <Spinner />
-            )}
-        </div>
-    );
+	return (
+		<div className="user-info">
+			<div className="info-header">
+				<Userimage width="5vw" />
+				<div className="info">
+					<p>{user.name}</p>
+					<br />
+					<br />
+					<span>
+						<b>99</b>
+						<p>posts </p>
+					</span>
+					<span>
+						<b>420</b>
+						<p>followers </p>
+					</span>
+					<span>
+						<b>69</b>
+						<p> following </p>
+					</span>
+					<br />
+					<br />
+					<p>Also known as pako and cko and mako and kako and taco</p>
+				</div>
+			</div>
+			<div className="post-container">
+				{!loading && posts !== null ? (
+					posts.map((post) => (
+						<PostsComponent key={post._id} post={post} />
+					))
+				) : (
+					<Spinner />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Userinfo;
